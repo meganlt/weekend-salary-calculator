2fe52dda2c8959e14814062b09114ea72a715bf9
Megan Tegeder <megan.tegeder@gmail.com>
2024-10-30 12:45:32 -0500

    updating with total monthly cost and css class

---
+++ script.js
@@ -0,0 +1,36 @@
+let totalMonthlyCost = 0;
+let maxMonthlyCost = 20000;
+
+function addEmployee(event){
+    event.preventDefault();
+
+    console.log('in addEmployee:', event);
+
+    let firstName = document.querySelector("#inputFirstName").value;
+    let lastName = document.querySelector("#inputLastName").value;
+    let id = document.querySelector("#inputId").value;
+    let title = document.querySelector("#inputTitle").value;
+    let salary = Number(document.querySelector("#inputSalary").value);
+    // Update Table with data
+    document.querySelector('#employeeTable').innerHTML += '<tr><td>'+ firstName +'</td><td>'+ lastName +'</td><td>'+ id +'</td><td>'+ title +'</td><td>'+ salary.toLocaleString('en-US') +'</td><td><button onclick="deleteEmployee()">Delete</button></td></tr>';
+    // Reset Form
+    document.getElementById('employeeForm').reset();
+    // Update Total
+    updateMonthlyCost(salary);
+    checkOverBudget();
+}
+
+// Function to check budget
+function checkOverBudget(){
+    if ( totalMonthlyCost > maxMonthlyCost ){
+       document.querySelector('footer').classList.add('over-budget');
+    }
+}
+
+// Function to Update Monthly Cost
+function updateMonthlyCost(amount){
+    console.log('in updateMonthlyCost:', amount);
+    totalMonthlyCost += Number(amount);
+    document.querySelector('#monthlyCostOutput').innerHTML = 'Total Monthly: ' + totalMonthlyCost.toLocaleString('en-US');
+}
+// Function to Delete Employee
